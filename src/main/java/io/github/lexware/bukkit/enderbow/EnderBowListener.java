a299968b1ab9bfc3519544de794a5540b3737c6b
Jamie Mansfield <malgm@lapis.blue>
2015-01-09 23:02:26 +0000

    Maybe this will work.

--- src/main/java/io/github/lexware/bukkit/enderbow/EnderBowListener.java
+++ src/main/java/io/github/lexware/bukkit/enderbow/EnderBowListener.java
@@ -7,6 +7,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
 
 /**
  * Created by jamie on 09/01/15.
@@ -21,15 +22,17 @@ public class EnderBowListener implements Listener {
     @EventHandler
     public void onEntityShootBowEvent(EntityShootBowEvent event) {
         if(event.getBow().getItemMeta().getDisplayName().equals("Ender bow")) {
-            ((Projectile) event.getProjectile()).setMetadata("enderBow", new FixedMetadataValue(plugin, "enderArrow"));
+            event.getProjectile().setMetadata("enderBowData", new FixedMetadataValue(plugin, "enderArrow"));
         }
     }
     
     @EventHandler
     public void onProjectileHit(ProjectileHitEvent event) {
-        if(event.getEntity().hasMetadata("enderBow") && event.getEntity().getMetadata("enderBow").contains("enderArrow")) {
-            if(event.getEntity().getShooter() instanceof Entity) {
-                ((Entity)event.getEntity().getShooter()).teleport(event.getEntity().getLocation());
+        if(event.getEntity().hasMetadata("enderBowData")) {
+            for(MetadataValue value : event.getEntity().getMetadata("enderBowData")) {
+                if(value.asString().equals("enderArrow")) {
+                    ((Entity)event.getEntity().getShooter()).teleport(event.getEntity().getLocation());
+                }
             }
         }
     }
