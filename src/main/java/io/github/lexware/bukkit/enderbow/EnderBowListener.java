8d248898584ce73d76bba8964d72839aba29c6ba
Jamie Mansfield <malgm@lapis.blue>
2015-01-09 22:19:09 +0000

    Todays work.

---
+++ src/main/java/io/github/lexware/bukkit/enderbow/EnderBowListener.java
@@ -0,0 +1,33 @@
+package io.github.lexware.bukkit.enderbow;
+
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+
+/**
+ * Created by jamie on 09/01/15.
+ */
+public class EnderBowListener implements Listener {
+    private final EnderBowPlugin plugin;
+
+    public EnderBowListener(EnderBowPlugin plugin) {
+        this.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onEntityShootBowEvent(EntityShootBowEvent event) {
+        if(event.getBow().getItemMeta().getDisplayName().equals("Ender bow")) {
+            ((Projectile) event.getProjectile()).setMetadata("enderBow", new FixedMetadataValue(plugin, "enderArrow"));
+        }
+    }
+    
+    @EventHandler
+    public void onProjectileHit(ProjectileHitEvent event) {
+        if(event.getEntity().hasMetadata("enderBow") && event.getEntity().getMetadata("enderBow").contains("enderArrow")) {
+            event.getEntity()._INVALID_getShooter().teleport(event.getEntity().getLocation());
+        }
+    }
+}
---
+++ src/main/java/io/github/lexware/bukkit/enderbow/EnderBowPlugin.java
@@ -0,0 +1,43 @@
+package io.github.lexware.bukkit.enderbow;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
+
+import java.io.IOException;
+
+/**
+ * Created by jamie on 09/01/15.
+ */
+public class EnderBowPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new EnderBowListener(this), this);
+
+        ItemStack enderBow = new ItemStack(Material.BOW);
+        ItemMeta itemMeta = enderBow.getItemMeta();
+        itemMeta.setDisplayName("Ender bow");
+        enderBow.setItemMeta(itemMeta);
+        ShapedRecipe enderBowRecipe = new ShapedRecipe(enderBow);
+        enderBowRecipe.shape("eee", "ebe", "eee");
+        enderBowRecipe.setIngredient('e', Material.ENDER_PEARL);
+        enderBowRecipe.setIngredient('b', Material.BOW);
+        
+        getServer().addRecipe(enderBowRecipe);
+
+        try {
+            Metrics metrics = new Metrics(this);
+            metrics.start();
+        } catch (IOException e) {
+            getLogger().info("Couldn't send Metrics data.");
+        }
+    }
+    
+    @Override
+    public void onDisable() {
+        getServer().resetRecipes();
+    }
+}
